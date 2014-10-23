03c63f8edef47dd322377c55334593572153ec46
Nam Hoang Luu <nam@mbearvn.com>
2014-10-23 19:19:06 +0700

    Add CommanderTrait

---
+++ src/Nam/Commander/CommanderTrait.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace Nam\Commander;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Input;
+use InvalidArgumentException;
+use ReflectionClass;
+
+
+/**
+ * Trait CommanderTrait
+ *
+ * @author  Nam Hoang Luu <nam@mbearvn.com>
+ * @package Nam\Commander
+ *
+ */
+trait CommanderTrait
+{
+    /**
+     * @param string $command
+     * @param array  $input
+     *
+     * @return mixed
+     */
+    protected function executeCommand($command, array $input = null)
+    {
+        $input = $input ?: Input::all();
+
+        $command = $this->mapInputToCommand($command, $input);
+        $commandBus = $this->getCommandBus();
+
+        return $commandBus->execute($command);
+    }
+
+    /**
+     * @param string $command
+     * @param array  $input
+     *
+     * @return BaseCommand
+     */
+    protected function mapInputToCommand($command, array $input)
+    {
+        $dependencies = [ ];
+
+        $class = new ReflectionClass($command);
+
+        foreach ($class->getConstructor()->getParameters() as $parameter) {
+            $name = $parameter->getName();
+
+            if (isset( $input[$name] )) {
+                $dependencies[] = $input[$name];
+            } elseif ($parameter->isDefaultValueAvailable()) {
+                $dependencies[] = $parameter->getDefaultValue();
+            } else {
+                throw new InvalidArgumentException("Unable to map input to command: {$name}");
+            }
+        }
+
+        return $class->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * @return CommandBus
+     */
+    protected function getCommandBus()
+    {
+        /** @var CommandBus $commandBus */
+        $commandBus = App::make('Nam\Commander\CommandBus');
+
+        return $commandBus;
+    }
+}
